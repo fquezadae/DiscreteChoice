28313dca87aafae61bd01a7132c031718d62f219
peterkuriyama <ptrkrym@uw.edu>
2017-05-03 11:04:17 -0700

    add spp correlation function

--- R/spp_corrs.R
+++ R/spp_corrs.R
@@ -18,15 +18,23 @@
 #Calculate species correlations
 
 spp_corrs <- function(tows = tows, output, two_species = c('dover sole', 'sablefish')){
-  
+browser()
   #Pull out the sampled tows from each 
   the_tows <- lapply(out$hauls, FUN = function(x) tows %>% filter(haul_id %in% x))
  
   filtered_tows <- lapply(the_tows, function(x) x %>% filter(species %in% two_species) %>% 
                      select(haul_id, apounds, species))
 
+  #Add in zeroes
+
+
+  #Standardize apounds, chosen over 
+  
+  filtered_tows[[1]] %>% group_by(species) %>% mutate(avg = mean(apounds), std = sd(apounds),
+    apounds_std = (apounds - avg) / std) -> filtered_tows[[1]]
+
   #cast the tows to plot them
-  casted <- lapply(filtered_tows, FUN = function(x) dcast(x, haul_id ~ species, value.var = 'apounds'))
+  casted <- lapply(filtered_tows, FUN = function(x) dcast(x, haul_id ~ species, value.var = 'apounds_std'))
   names(casted) <- 1:length(casted)
 
   #Replace the NAs with 0
@@ -35,6 +43,22 @@ spp_corrs <- function(tows = tows, output, two_species = c('dover sole', 'sablef
   casted[is.na(casted[, 3]), 3] <- 0
   casted[is.na(casted[, 4]), 4] <- 0
   
+  #Normalize the zeroes also
+  for_norm <- filtered_tows[[1]] %>% group_by(species) %>% summarize(avg = unique(avg), std = unique(std))
+
+  #Normalize Species 1
+  spp1_col <- which(names(casted) %in% two_species[1])
+  spp1_norms <- for_norm %>% filter(species == two_species[1])
+  #
+  casted[which(casted[, spp1_col] == 0), spp1_col] <- casted[which(casted[, spp1_col] == 0), spp1_col]
+  hist(casted[, 3], breaks = 30)
+  hist(casted[, 4], breaks = 30)
+  plot(casted[, 3], casted[, 4], pch = 19)
+  cor(casted[, 3], casted[, 4])
+
+
+  casted[which(casted[, 3] == 0), 3]
+
   #Calculate correlations between pairs
   names(casted)[3] <- 'spp1'
   names(casted)[4] <- 'spp2'
