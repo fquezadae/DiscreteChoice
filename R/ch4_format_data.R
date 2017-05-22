e66f28fa6b2e3f2a21ce42f18882b201884edb16
peterkuriyama <ptrkrym@uw.edu>
2017-05-22 15:50:38 -0700

    add permutation tests as a function

--- R/ch4_format_data.R
+++ R/ch4_format_data.R
@@ -46,170 +46,78 @@ ch4_format_data <- function(input){
     unq_clust)
   
   #--------------------------------------------------------------------------------
-  #CHANGES IN TOWS IN CLUSTERS
-  #before after changes with permutation test
-  
-  #look at top 100 clusters; ntows column is number of unique hauls
-  clust_tows <- clust_tows %>% arrange(desc(ntows))
-  top100 <- unique(clust_tows$unq_clust)[1:100]
-  ntow_perm <- clust_tows %>% group_by(dyear, unq_clust) %>% summarize(ntows = length(species)) 
-  
-  p_vals <- rep(999, 100)
-  
-  for(ii in 1:100){
-    temp <- ntow_perm %>% filter(unq_clust == top100[ii], dyear > 2007)
-    if(length(temp$dyear) != 6) next
-    bef <- mean(temp$ntows[1:3])
-    aft <- mean(temp$ntows[4:6])
-    emp_out <- aft - bef
-    
-    resamp <- sapply(1:1000, FUN = function(x){
-      draw <- sample(temp$ntows, replace = F)
-      bef <- mean(draw[1:3])
-      aft <- mean(draw[4:6])
-      out <- aft - bef
-      return(out)
-    })
-  
-    #To look at specific distributions
-    # hist(resamp)  
-    # abline(v = emp_out, lwd = 2, lty = 2, col = 'red')  
-  
-    #p_value of emp_out, which things had decreases?
-    p_vals[ii] <- length(which(resamp <= emp_out)) / length(resamp)
-  }
-  
-  sigs <- data.frame(p_vals = p_vals, sig = "999")
-  sigs$sig <- as.numeric(sigs$sig)
-  sigs$sig <- 'no change'
-  sigs[(sigs$p_vals <= .05), "sig"] <- 'sig decrease'
-  sigs[(sigs$p_vals >= .95), "sig"] <- 'sig increase'
-  sigs[(sigs$p_vals == 999), "sig"] <- 'not enough years'
-  sigs$unq_clust <- top100
-  
-  #Combine with clust_tows
-  top100_clusts <- inner_join(clust_tows, sigs, by = c('unq_clust'))
-  top100_clusts <- plyr::rename(top100_clusts, c("p_vals" = 'p_vals_tows', 'sig' = 'sig_tows'))
-  
-  #Check plots
-  # ggplot(top100_clusts) + 
-  #   geom_segment(aes(x = set_long, xend = up_long, y = set_lat, yend = up_lat, colour = clust)) + 
-  #   facet_wrap(~ sig_tows)
-  
-  #--------------------------------------------------------------------------------
-  #CHANGES IN UNIQUE VESSELS IN CLUSTERS
-  
-  #look at top 100 clusters, keep everything in top100_clusts
-  top100_clusts <- top100_clusts %>% group_by(unq_clust) %>% mutate(nvess = length(unique(drvid))) %>%
+  #Permutation tests in each cluster
+  #Group values by year and cluster
+  #---------------------------------------
+  #Number of tows
+  clust_tows <- ch4_perm_test(input = clust_tows, column = 'ntows', ndraws = 1000)
+
+  #---------------------------------------
+  #Number of vessels
+  clust_tows <- clust_tows %>% group_by(dyear, unq_clust) %>% mutate(nvess = length(unique(drvid))) %>% 
     as.data.frame
-  
-  # top100 <- unique(clust_tows$unq_clust)[1:100]
-  clust_vess2 <- top100_clusts %>% group_by(dyear, unq_clust) %>% summarize(nvess = length(unique(drvid))) %>% 
-    as.data.frame
-  
-  # nvess_perm <- clust_tows %>% group_by(dyear, unq_clust) %>% summarize(ntows = length(species)) 
-  p_vals <- rep(999, 100)
-  
-  for(ii in 1:100){
-    temp <- clust_vess2 %>% filter(unq_clust == top100[ii], dyear > 2007)
-    if(length(temp$dyear) != 6) next
-    bef <- mean(temp$nvess[1:3])
-    aft <- mean(temp$nvess[4:6])
-    emp_out <- aft - bef
-    
-    resamp <- sapply(1:1000, FUN = function(x){
-      draw <- sample(temp$nvess, replace = F)
-      bef <- mean(draw[1:3])
-      aft <- mean(draw[4:6])
-      out <- aft - bef
-      return(out)
-    })
-  
-    #To look at specific distributions
-    # hist(resamp)  
-    # abline(v = emp_out, lwd = 2, lty = 2, col = 'red')  
-  
-    #p_value of emp_out, which things had decreases?
-    p_vals[ii] <- length(which(resamp <= emp_out)) / length(resamp)
-  }
-  
-  sigs <- data.frame(p_vals = p_vals, sig = "999")
-  sigs$sig <- as.numeric(sigs$sig)
-  sigs$sig <- 'no change'
-  sigs[(sigs$p_vals <= .05), "sig"] <- 'sig decrease'
-  sigs[(sigs$p_vals >= .95), "sig"] <- 'sig increase'
-  sigs[(sigs$p_vals == 999), "sig"] <- 'not enough years'
-  
-  #Combine with clust_tows
-  sigs$unq_clust <- top100
-  top100_clusts <- inner_join(top100_clusts, sigs, by = c('unq_clust'))
-  top100_clusts <- plyr::rename(top100_clusts, c("p_vals" = 'p_vals_vess', 'sig' = 'sig_vess'))
-  
-  # ggplot(top100_clusts) + 
-  #   geom_segment(aes(x = set_long, xend = up_long, y = set_lat, yend = up_lat, colour = clust)) + 
-  #   facet_wrap(~ sig_vess + sig_tows)
-  
+  clust_tows <- ch4_perm_test(input = clust_tows, column = 'nvess', ndraws = 1000)
+    #--------------------------------------------------------------------------------
   #Add in before/after catch shares column
-  top100_clusts$when <- 'before'
-  top100_clusts[which(top100_clusts$dyear > 2010), 'when'] <- 'after'
-  top100_clusts$when <- factor(top100_clusts$when, levels = c('before', 'after'))
+  clust_tows$when <- 'before'
+  clust_tows[which(clust_tows$dyear > 2010), 'when'] <- 'after'
+  clust_tows$when <- factor(clust_tows$when, levels = c('before', 'after'))
   
   #Add category for species
-  
-  top100_clusts$type <- "not important"
-  unique(top100_clusts$species)[unique(top100_clusts$species) %>% order]
+  clust_tows$type <- "not important"
+  # unique(clust_tows$species)[unique(clust_tows$species) %>% order]
   
   #Targets, weak, managed
   weaks <- c("Darkblotched Rockfish", "Pacific Ocean Perch", "Canary Rockfish",
     "Bocaccio Rockfish", "Yelloweye Rockfish", "Cowcod Rockfish", "Cowcod")
-  top100_clusts[which(top100_clusts$species %in% weaks), 'type'] <- 'weaks'
+  clust_tows[which(clust_tows$species %in% weaks), 'type'] <- 'weaks'
   targs <- c("Dover Sole", 'Sablefish', 'Longspine Thornyhead', "Petrale Sole",
     "Lingcod", "Shortspine Thornyhead")
-  top100_clusts[which(top100_clusts$species %in% targs), 'type'] <- 'targets'
+  clust_tows[which(clust_tows$species %in% targs), 'type'] <- 'targets'
   groundfish <- c('Arrowtooth Flounder', 'English Sole', 'Longnose Skate', 'Yellowtail Rockfish',
     "Widow Rockfish", 'Chilipepper Rockfish', 'Black Rockfish', "Vermilion Rockfish", 'Greenstriped Rockfish',
     "Greenspotted Rockfish", 'Bank Rockfish')
-  top100_clusts[which(top100_clusts$species %in% groundfish), 'type'] <- 'groundfish'
-  top100_clusts %>% filter(type == 'not important') %>% distinct(species) %>% arrange()
+  clust_tows[which(clust_tows$species %in% groundfish), 'type'] <- 'groundfish'
+  clust_tows %>% filter(type == 'not important') %>% distinct(species) %>% arrange()
   
   #Remove "clust" and only keep unq_clust
-  top100_clusts$clust <- NULL
+  clust_tows$clust <- NULL
   
   #---------------------------------------------------------------------------------
   #----Analyze changes for catch compositions, by tow and trip
   
   #Done for hpounds
-  top100_clusts %>% group_by(haul_id) %>% mutate(tot = sum(hpounds)) %>% 
-    group_by(haul_id, species) %>% mutate(perc = hpounds / tot ) %>% as.data.frame -> top100_clusts
+  clust_tows %>% group_by(haul_id) %>% mutate(tot = sum(hpounds)) %>% 
+    group_by(haul_id, species) %>% mutate(perc = hpounds / tot ) %>% as.data.frame -> clust_tows
   
   #Histograms across all species of catch percentages
 
-  # top100_clusts %>% filter(type == 'targets') %>% ggplot(aes(x = perc)) + 
+  # clust_tows %>% filter(type == 'targets') %>% ggplot(aes(x = perc)) + 
   #   geom_histogram() + facet_wrap(~ species + when, ncol = 2)
-  # top100_clusts %>% filter(type == 'weaks') %>% ggplot(aes(x = perc)) + 
+  # clust_tows %>% filter(type == 'weaks') %>% ggplot(aes(x = perc)) + 
   #   geom_histogram() + facet_wrap(~ species + when, ncol = 2)
   
   #Correlation between total pounds and catch percentage of species
   
   #Add in proportion of zero and skew for each  species
-  top100_clusts <- top100_clusts %>% group_by(unq_clust, species) %>% mutate(ntows_with = length(unique(haul_id)), 
+  clust_tows <- clust_tows %>% group_by(unq_clust, species) %>% mutate(ntows_with = length(unique(haul_id)), 
     prop_zero = 1 - (ntows_with / ntows), skew = calc_skew(log(hpounds))) %>% as.data.frame
   
   #ID selected species, and frequency of high bycatch
-  # top100_clusts %>% filter(species == 'Petrale Sole') %>% ggplot(aes(x = prop_zero, 
+  # clust_tows %>% filter(species == 'Petrale Sole') %>% ggplot(aes(x = prop_zero, 
   #   y = skew)) + geom_point(aes(colour = unq_clust))
   
-  top100_clusts$selected <- "no"
-  top100_clusts[which(top100_clusts$prop_zero <= 0.5 & top100_clusts$skew <= 0), 'selected'] <- "yes"
+  clust_tows$selected <- "no"
+  clust_tows[which(clust_tows$prop_zero <= 0.5 & clust_tows$skew <= 0), 'selected'] <- "yes"
   
   #ID frequency of high bycatch
-  top100_clusts$high_perc <- 'no'
-  top100_clusts[which(top100_clusts$perc >= 0.5 & top100_clusts$type == 'weaks'), 
+  clust_tows$high_perc <- 'no'
+  clust_tows[which(clust_tows$perc >= 0.5 & clust_tows$type == 'weaks'), 
     'high_perc'] <- "yes"
-  top100_clusts <- top100_clusts %>% group_by(unq_clust) %>% 
+  clust_tows <- clust_tows %>% group_by(unq_clust) %>% 
     mutate(nhigh = length(which(high_perc == 'yes')), 
       high_weak_perc = unique(nhigh / ntows)) %>% as.data.frame %>% select(-nhigh, -high_perc)
   
-  return(top100_clusts)  
+  return(clust_tows)  
 
 }
