acdb077fb7add553e8a6e6ebf871f9e84c55557a
peterkuriyama <ptrkrym@uw.edu>
2017-07-30 12:17:15 -0700

    add plot pairs function

--- R/plot_pairs.R
+++ R/plot_pairs.R
@@ -20,8 +20,13 @@ plot_pairs <- function(var_pp = "spp_clust_catch", save_pp = TRUE){
     dcast(unq_clust + ntows ~ species) 
   names(clust_catch) <- gsub(" ", "_", names(clust_catch))
 
-
   filename <- paste0("figs/pairs_", var_pp, ".png")
+  
+  #if save_pp == FALSE, just pring
+  if(save_pp == FALSE){
+    print(ggpairs(clust_catch %>% select(-unq_clust, -ntows)))
+  }
+
   if(save_pp == TRUE){
     png(width = 15.5, height = 15.5, file = filename, units = 'in', res = 200)
     print(ggpairs(clust_catch %>% select(-unq_clust, -ntows)))
