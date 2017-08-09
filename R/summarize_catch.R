62331c2bb4f7058461f5b5bc87314290fd861776
peterkuriyama <ptrkrym@uw.edu>
2017-08-09 14:49:13 -0700

    change way that catches are processed

--- R/summarize_catch.R
+++ R/summarize_catch.R
@@ -6,17 +6,28 @@
 
 #' @export
 
-summarize_catch <- function(clust = filt_clusts, haul_id1){
-
+summarize_catch <- function(clust = filt_clusts, haul_id1){  
   #Pull out the specific tow
-  tow <- clust %>% filter(haul_id == haul_id1)
-  
-  catch <- tow %>% group_by(species, type) %>% summarize(hpounds = sum(hpounds, na.rm = T),
-    haul_value = unique(haul_value), haul_profit = unique(haul_profit), 
-    profit_fuel_only = unique(profit_fuel_only), unq_clust_bin = unique(unq_clust_bin), 
-    xbin = unique(xbin), ybin = unique(ybin), unq = unique(unq),
-    haul_id = unique(haul_id), unq_clust = unique(unq_clust),
-    avg_long = unique(avg_long), avg_lat = unique(avg_lat)) %>% as.data.frame
+  # tow <- clust %>% filter(haul_id == haul_id1)
+  catch <- clust %>% group_by(haul_id, species, type) %>% summarize(hpounds = sum(hpounds, na.rm = T),
+    haul_value = unique(haul_value),  haul_profit = unique(haul_profit),
+    profit_fuel_only = unique(profit_fuel_only), unq_clust_bin = unique(unq_clust_bin),
+    xbin = unique(xbin), ybin = unique(ybin), unq = unique(unq), 
+    unq_clust = unique(unq_clust), avg_long = unique(avg_long), 
+    avg_lat = unique(avg_lat)) %>% as.data.frame
+
+  haul_ids <- data.frame(haul_id = haul_id1, tow_num = 1:length(haul_id1))
+  haul_ids$haul_id <- as.character(haul_ids$haul_id)
+
+  catch <- catch %>% left_join(haul_ids, by = 'haul_id')
+  catch <- catch %>% arrange(tow_num)
+
+  # catch <- tow %>% group_by(species, type) %>% summarize(hpounds = sum(hpounds, na.rm = T),
+  #   haul_value = unique(haul_value), haul_profit = unique(haul_profit), 
+  #   profit_fuel_only = unique(profit_fuel_only), unq_clust_bin = unique(unq_clust_bin), 
+  #   xbin = unique(xbin), ybin = unique(ybin), unq = unique(unq),
+  #   haul_id = unique(haul_id), unq_clust = unique(unq_clust),
+  #   avg_long = unique(avg_long), avg_lat = unique(avg_lat)) %>% as.data.frame
 
   return(catch)
 
