95e41cd95c69b5cbaa301a8e3a300dbd759d1a53
peterkuriyama <ptrkrym@uw.edu>
2017-09-14 15:44:38 -0700

    sample hauls with foreach instead of mclapply

--- R/sampled_rums.R
+++ R/sampled_rums.R
@@ -91,43 +91,55 @@ sampled_rums <- function(data_in = filt_clusts, the_port = "ASTORIA / WARRENTON"
   #Sample hauls and calculate distances
   #For each haul in the focus year, sample nhauls_sampled tows
 
-  sampled_hauls <- mclapply(1:nrow(hauls), FUN = function(xx){
-    the_samples <- dist_hauls_catch_shares %>% filter(haul_id != hauls[xx, 'haul_id']) %>% 
-      sample_n(size = nhauls_sampled, replace = F)
   
-    #Now calculate the distances between the points and the actual points
-    actual_haul <- hauls[xx, ]
+  cl <- makeCluster(ncores)
+  registerDoParallel(cl)
+
+  sampled_hauls <- foreach::foreach(ii = 1:nrow(hauls), 
+    .export = c("sample_hauls", 'deg2rad', 'gcd_slc'), 
+    .packages = c("dplyr", 'lubridate')) %dopar% 
+      sample_hauls(xx = ii, hauls1 = hauls, 
+        dist_hauls_catch_shares1 = dist_hauls_catch_shares, nhauls_sampled1 = nhauls_sampled)
+  stopCluster(cl)
+
+
+  # sampled_hauls <- mclapply(1:nrow(hauls), FUN = function(xx){
+  #   the_samples <- dist_hauls_catch_shares %>% filter(haul_id != hauls[xx, 'haul_id']) %>% 
+  #     sample_n(size = nhauls_sampled, replace = F)
   
-    #calculate distances in km
-    prev_point <- actual_haul %>% select(prev_avg_long, prev_avg_lat)
-    the_samples$prev_avg_long <- prev_point$prev_avg_long
-    the_samples$prev_avg_lat <- prev_point$prev_avg_lat
-    the_samples[, c('avg_long', 'avg_lat', 'prev_avg_lat', 'prev_avg_long')] <- deg2rad(the_samples[, 
-      c('avg_long', 'avg_lat', 'prev_avg_lat', 'prev_avg_long')])
+  #   #Now calculate the distances between the points and the actual points
+  #   actual_haul <- hauls[xx, ]
   
-    the_samples$distance <- gcd_slc(the_samples$prev_avg_long, 
-        the_samples$prev_avg_lat, the_samples$avg_long, the_samples$avg_lat)
+  #   #calculate distances in km
+  #   prev_point <- actual_haul %>% select(prev_avg_long, prev_avg_lat)
+  #   the_samples$prev_avg_long <- prev_point$prev_avg_long
+  #   the_samples$prev_avg_lat <- prev_point$prev_avg_lat
+  #   the_samples[, c('avg_long', 'avg_lat', 'prev_avg_lat', 'prev_avg_long')] <- deg2rad(the_samples[, 
+  #     c('avg_long', 'avg_lat', 'prev_avg_lat', 'prev_avg_long')])
   
-    #Calculate distance of empirical haul
-    actual_haul[, c('avg_long', 'avg_lat', 'prev_avg_lat', 'prev_avg_long')] <- deg2rad(actual_haul[, 
-        c('avg_long', 'avg_lat', 'prev_avg_lat', 'prev_avg_long')])  
-    actual_haul$distance <- gcd_slc(actual_haul$prev_avg_long, 
-        actual_haul$prev_avg_lat, actual_haul$avg_long, actual_haul$avg_lat)
+  #   the_samples$distance <- gcd_slc(the_samples$prev_avg_long, 
+  #       the_samples$prev_avg_lat, the_samples$avg_long, the_samples$avg_lat)
   
-    #Combine the sampled values and the empirical haul
-    actual_haul$prev_haul_num <- NULL
-    actual_haul$fished <- TRUE
-    actual_haul$fished_haul <- actual_haul$haul_id
-    the_samples$fished <- FALSE
-    the_samples$fished_haul <- actual_haul$haul_id
+  #   #Calculate distance of empirical haul
+  #   actual_haul[, c('avg_long', 'avg_lat', 'prev_avg_lat', 'prev_avg_long')] <- deg2rad(actual_haul[, 
+  #       c('avg_long', 'avg_lat', 'prev_avg_lat', 'prev_avg_long')])  
+  #   actual_haul$distance <- gcd_slc(actual_haul$prev_avg_long, 
+  #       actual_haul$prev_avg_lat, actual_haul$avg_long, actual_haul$avg_lat)
   
-    the_samples <- rbind(actual_haul, the_samples)
+  #   #Combine the sampled values and the empirical haul
+  #   actual_haul$prev_haul_num <- NULL
+  #   actual_haul$fished <- TRUE
+  #   actual_haul$fished_haul <- actual_haul$haul_id
+  #   the_samples$fished <- FALSE
+  #   the_samples$fished_haul <- actual_haul$haul_id
   
-    #Define the set_date
-    the_samples$set_date <- ymd(paste(actual_haul$set_year, actual_haul$set_month, actual_haul$set_day, sep = "-"))
+  #   the_samples <- rbind(actual_haul, the_samples)
+  
+  #   #Define the set_date
+  #   the_samples$set_date <- ymd(paste(actual_haul$set_year, actual_haul$set_month, actual_haul$set_day, sep = "-"))
     
-    return(the_samples)
-  }, mc.cores = ncores)
+  #   return(the_samples)
+  # }, mc.cores = ncores)
 
   print("Done sampling hauls")  
   sampled_hauls <- ldply(sampled_hauls)
