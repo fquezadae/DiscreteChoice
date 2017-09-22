74df4b0739a10f9949c9bb909da81ecaf44006e5
peterkuriyama <ptrkrym@uw.edu>
2017-09-22 13:52:31 -0700

    update the script with xtable

--- figs/ch4_rum_table.R
+++ figs/ch4_rum_table.R
@@ -30,6 +30,24 @@ lapply(1:8, FUN = function(xx){
 } )
 
 #Try foramtting this shit in xtable
+c1_table <- lapply(coefs1, FUN = function(xx){
+  return(xx$value)
+})
+
+c1_table <- ldply(c1_table)
+names(c1_table) <- c('port', rownames(coefs1[[1]]))
+c1_table <- t(c1_table)
+c1_table <- as.data.frame(c1_table)
+
+names(c1_table) <- port_names
+
+print(xtable(c1_table), file = 'figs/coefs1.tex')
+
+
+as.vector(c1_table[1, ])
+
+coefs1$c
+
 coefs_table <- 
 
 xtable()
