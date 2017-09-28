0ed17c7eb80d7cffb61c8728f01c4abea4a6a6de
peterkuriyama <ptrkrym@uw.edu>
2017-09-28 11:29:34 -0700

    add file

--- R/port_rums.R
+++ R/port_rums.R
@@ -15,32 +15,32 @@
 port_rums <- function(m_y,
   f_y, nhauls_sampled = 75,
   seed, ncores, r_c = 1, r_s = 10){
-# library(beepr)
-#   runs1 <- lapply(1:length(ports), FUN = function(yy){
-#     st_time <- Sys.time()
-#     rum <- sampled_rums(data_in = tows_clust, the_port = ports[[yy]],
-#                         min_year = m_y, max_year = f_y,
-#                         risk_coefficient = r_c, ndays = 30, focus_year = f_y, 
-#                         nhauls_sampled = nhauls_sampled,
-#                         seed = seed, ncores = ncores, rev_scale = r_s)
-#     r_time <- Sys.time() - st_time
-#     print(r_time)
-#     print(rum[[1]])
-# beep()    
-#     return(rum)
-#   })
-  
-#   ports_names <- lapply(ports, FUN = function(yy) paste(yy, collapse = "_"))
-#   ports_names <- ldply(ports_names)
-#   ports_names <- as.vector(ports_names$V1)
-  
-#   coefs1 <- lapply(runs1, FUN = function(xx) xx[[1]])
-#   names(coefs1) <- ports_names
-browser()
-  filename <- paste0("coefs", r_c, "_rev", r_s, '_focyr', f_y)
-  save(coefs1, file = paste0("//udrive.uw.edu//udrive//", filename, ".Rdata"))
-  filename <- paste0("runs", r_c, "_rev", r_s, '_focyr', f_y)
-  save(runs1, file = paste0("//udrive.uw.edu//udrive//", filename, ".Rdata"))
 
-  return(coefs1)
+  #Run the models
+  runs <- lapply(1:length(ports), FUN = function(yy){
+    st_time <- Sys.time()
+    rum <- sampled_rums(data_in = tows_clust, the_port = ports[[yy]],
+                        min_year = m_y, max_year = f_y,
+                        risk_coefficient = r_c, ndays = 30, focus_year = f_y, 
+                        nhauls_sampled = nhauls_sampled,
+                        seed = seed, ncores = ncores, rev_scale = r_s)
+    r_time <- Sys.time() - st_time
+    print(r_time)
+    print(rum[[1]])
+    return(rum)
+  })
+  
+  ports_names <- lapply(ports, FUN = function(yy) paste(yy, collapse = "_"))
+  ports_names <- ldply(ports_names)
+  ports_names <- as.vector(ports_names$V1)
+  
+  coefs <- lapply(runs, FUN = function(xx) xx[[1]])
+  names(coefs) <- ports_names
+
+  filename <- paste0("coefs", r_c, "_rev", r_s, '_focyr', f_y)
+  save(coefs, file = paste0("//udrive.uw.edu//udrive//", filename, ".Rdata"))
+  filename <- paste0("runs", r_c, "_rev", r_s, '_focyr', f_y)
+  save(runs, file = paste0("//udrive.uw.edu//udrive//", filename, ".Rdata"))
+
+  return(coefs)
 }
