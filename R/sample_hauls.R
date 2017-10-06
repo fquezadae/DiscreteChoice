01f6edccd319e456841da3acec4c653a00d7f21c
peterkuriyama <ptrkrym@uw.edu>
2017-10-06 16:51:25 -0700

    use start and end points rather than average points

--- R/sample_hauls.R
+++ R/sample_hauls.R
@@ -12,7 +12,7 @@
 
   sample_hauls <- function(xx, hauls1 = hauls, dist_hauls_catch_shares1 = dist_hauls_catch_shares,
     nhauls_sampled1 = 50, depth_bin_proportions, the_seed){
-
+browser()
    #Seed uses the row index in the foreach function of sampled_rums
    set.seed(the_seed)
    
@@ -33,21 +33,33 @@
     actual_haul <- hauls1[xx, ]
   
     #calculate distances in km
-    prev_point <- actual_haul %>% select(prev_avg_long, prev_avg_lat)
-    the_samples$prev_avg_long <- prev_point$prev_avg_long
-    the_samples$prev_avg_lat <- prev_point$prev_avg_lat
-    the_samples[, c('avg_long', 'avg_lat', 'prev_avg_lat', 'prev_avg_long')] <- deg2rad(the_samples[, 
-      c('avg_long', 'avg_lat', 'prev_avg_lat', 'prev_avg_long')])
+    prev_point <- actual_haul %>% select(prev_up_long, prev_up_lat)
+    the_samples$prev_up_long <- prev_point$prev_up_long
+    the_samples$prev_up_lat <- prev_point$prev_up_lat
+    the_samples[, c('set_long', 'set_lat', 'prev_up_lat', 'prev_up_long')] <- deg2rad(the_samples[, 
+      c('set_long', 'set_lat', 'prev_up_lat', 'prev_up_long')])
+    
+    # the_samples$prev_avg_long <- prev_point$prev_avg_long
+    # the_samples$prev_avg_lat <- prev_point$prev_avg_lat
+    # the_samples[, c('avg_long', 'avg_lat', 'prev_avg_lat', 'prev_avg_long')] <- deg2rad(the_samples[, 
+    #   c('avg_long', 'avg_lat', 'prev_avg_lat', 'prev_avg_long')])
   
-    the_samples$distance <- gcd_slc(the_samples$prev_avg_long, 
-        the_samples$prev_avg_lat, the_samples$avg_long, the_samples$avg_lat)
-  
+    # the_samples$distance <- gcd_slc(the_samples$prev_avg_long, 
+    #     the_samples$prev_avg_lat, the_samples$avg_long, the_samples$avg_lat)
+    
+    the_samples$distance <- gcd_slc(the_samples$prev_up_long, 
+        the_samples$prev_up_lat, the_samples$set_long, the_samples$set_lat)
+    
     #Calculate distance of empirical haul
-    actual_haul[, c('avg_long', 'avg_lat', 'prev_avg_lat', 'prev_avg_long')] <- deg2rad(actual_haul[, 
-        c('avg_long', 'avg_lat', 'prev_avg_lat', 'prev_avg_long')])  
-    actual_haul$distance <- gcd_slc(actual_haul$prev_avg_long, 
-        actual_haul$prev_avg_lat, actual_haul$avg_long, actual_haul$avg_lat)
-  
+    # actual_haul[, c('avg_long', 'avg_lat', 'prev_avg_lat', 'prev_avg_long')] <- deg2rad(actual_haul[, 
+    #     c('avg_long', 'avg_lat', 'prev_avg_lat', 'prev_avg_long')])  
+    # actual_haul$distance <- gcd_slc(actual_haul$prev_avg_long, 
+    #     actual_haul$prev_avg_lat, actual_haul$avg_long, actual_haul$avg_lat)
+    actual_haul[, c('set_long', 'set_lat', 'prev_up_lat', 'prev_up_long')] <- deg2rad(actual_haul[, 
+        c('set_long', 'set_lat', 'prev_up_lat', 'prev_up_long')])  
+    actual_haul$distance <- gcd_slc(actual_haul$prev_up_long, 
+        actual_haul$prev_up_lat, actual_haul$set_long, actual_haul$set_lat)
+
     #Combine the sampled values and the empirical haul
     actual_haul$prev_haul_num <- NULL
     actual_haul$fished <- TRUE
