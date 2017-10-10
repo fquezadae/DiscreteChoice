e45c385d0dbda62163960501ef9e07055da3e54c
peterkuriyama <ptrkrym@uw.edu>
2017-10-09 21:41:58 -0700

    add value

--- R/process_dummys2.R
+++ R/process_dummys2.R
@@ -74,7 +74,7 @@ rev_distance <- 5
     dum_rev_dollars <- managed_val - weak_val
   }
 
-  outs <- data_frame(dummy_prev_days = dum30, dummy_prev_year_days = dum30y, 
+  outs <- data_frame(dummy_prev_days = dum30_val, dummy_prev_year_days = dum30y_val, 
     dummy_miss = dum_rev_val, miss_rev = dum_rev_dollars)
 
   #-----------------------------------------------------------------------------------------------
