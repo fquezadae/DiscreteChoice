e6f1bde0e885f420152e55aa95ad65eecc1f6917
peterkuriyama <ptrkrym@uw.edu>
2017-10-10 14:58:01 -0700

    update

--- figs/ch4_rum_table.R
+++ figs/ch4_rum_table.R
@@ -3,86 +3,6 @@
 library(xtable)
 
 
-
-# ' Prints a LaTeX table with numeric columns aligned on their decimal points.
-# ' 
-# ' This function wraps the \code{\link{xtable}} and \code{\link{print.xtable}}
-# ' functions in the \code{xtable} package so that numeric columns are aligned
-# ' on their decimal place.
-# ' 
-# ' See \url{http://jason.bryer.org/posts/2013-01-04/xtable_with_aligned_decimals.html}
-# ' for more information.
-# ' 
-# ' @author Jason Bryer <jason@@bryer.org>
-# ' @param x a data frame to create a LaTeX table from.
-# ' @param cols a numeric vector indicating which columns should be aligned on
-# '        decimal points. It defaults to all columns of type numeric.
-# ' @param colAlignment named character vector where each element name corresponds to a
-#         column name and the value is the LaTeX alignment (i.e. l, r, or c).
-# ' @param tocharFun the function used to convert the numeric vecotr to a character
-# '        vector. This defaults to \code{\link{prettyNum}}, but other possible
-# '        options are \code{\link{as.character}}, \code{\link{format}}, 
-# '        \code{\link{formatC}}, or some other custom function.
-# ' @param ... other parameters passed to \code{tocharFun}, \code{\link{xtable}},
-# '        and \code{\link{print.xtable}}.
-# ' @seealso xtable
-# ' @export
-xtable.decimal <- function(x, 
-      cols=which(lapply(x, class) == 'numeric'), 
-      colAlignment, 
-      tocharFun=prettyNum,
-      ...) {
-  splitCol <- function(x, ...) {
-    s <- strsplit(tocharFun(x, ...), split='.', fixed=TRUE)
-    right <- sapply(s, FUN=function(x) { ifelse(length(x) == 2, x[2], '0') })
-    left <- sapply(s, FUN=function(x) { x[1] })
-    data.frame(left=left, right=right, stringsAsFactors=FALSE)
-  }
-
-  cols <- cols[order(cols, decreasing=TRUE)]
-  colnames <- names(x)
-  for(i in cols) {
-    if(i == 1) {
-      tmp <- cbind(splitCol(x[,1], ...), x[,2:ncol(x)])
-      names(tmp)[1:2] <- paste(names(tmp)[1], c('left','right'), sep='.')
-      names(tmp)[3:ncol(x)] <- names(x)[2:ncol(x)]
-      x <- tmp
-    } else if(i == ncol(x)) {
-      tmp <- cbind(x[,1:(ncol(x)-1)], splitCol(x[,ncol(x)], ...))
-      names(tmp)[1:(ncol(tmp)-2)] <- names(x)[1:(ncol(x)-1)]
-      names(tmp)[(ncol(tmp)-1):ncol(tmp)] <- paste(names(x)[ncol(x)], 
-            c('left','right'), sep='.')
-      x <- tmp
-    } else {
-      tmp <- cbind(x[,1:(i-1)], splitCol(x[,i], ...), x[,(i+1):ncol(x)])
-      names(tmp)[1:(i-1)] <- names(x)[1:(i-1)]
-      names(tmp)[i:(i+1)] <- paste(names(x)[i], c('left','right'), sep='.')
-      names(tmp)[(i+2):ncol(tmp)] <- names(x)[(i+1):ncol(x)]
-      x <- tmp
-    }
-  }
-
-  colnames[cols] <- paste('\\multicolumn{2}{c}{', colnames[cols], '}', sep='')
-  colnames <- paste(colnames, collapse=' & ')
-
-  addtorow <- list()
-  addtorow$pos <- list()
-  addtorow$pos[[1]] <- c(0)
-  addtorow$command <- paste( colnames, ' \\\\ ', sep='')
-
-  align <- rep('l', ncol(x))
-  if(!missing(colAlignment)) {
-    for(i in seq_along(colAlignment)) {
-      align[names(x) == names(colAlignment)[i]] <- colAlignment[i]
-    }
-  }
-  align[grep('.left$', names(x), perl=TRUE)] <- 'r@{.}'
-  align <- c('l', align) #Add an alignment for row names
-
-  xtab <- xtable(x, align=align, ...)
-  print(xtab, add.to.row=addtorow, include.rownames=FALSE, include.colnames=FALSE, ...)
-}
-
 #-----------------------------------------------------------------
 #Load data 
 load("/Volumes/udrive/coefs1.Rdata")
@@ -97,34 +17,97 @@ names(coefs50) <- port_names
 
 #-----------------------------------------------------------------
 #Paste significance values to 
+#-------------------------------------------------------------------------------------
 
-coefs1 <- lapply(coefs1, FUN = function(xx){
-  xx$value = paste(formatC(xx$coefs, digits = 5, format = 'f'), xx$significance)
-  return(xx)
-} )
+load("/Volumes/udrive/coefs1_rev10_minyr2009_focyr2011_nports7_seed305.Rdata")
+coefs1 <- coefs
 
-#Calculate difference between tables
-lapply(1:8, FUN = function(xx){
-  temp <- coefs50[[xx]]$coefs - coefs1[[xx]]$coefs
-  coefs1[[xx]]$change <- temp
-  return(coefs1[[xx]])
-} )
+load("/Volumes/udrive/coefs1_rev10_minyr2010_focyr2012_nports7_seed305.Rdata")
+coefs2 <- coefs
 
-#Try foramtting this shit in xtable
-c1_table <- lapply(coefs1, FUN = function(xx){
-  return(xx$value)
+load("/Volumes/udrive/coefs1_rev10_minyr2011_focyr2013_nports7_seed305.Rdata")
+coefs3 <- coefs
+
+load("/Volumes/udrive/coefs1_rev10_minyr2011_focyr2014_nports7_seed305.Rdata")
+coefs4 <- coefs
+
+
+tbl1 <- format_coefs(coefs = coefs1)
+tbl2 <- format_coefs(coefs = coefs2)
+tbl3 <- format_coefs(coefs = coefs3)
+tbl4 <- format_coefs(coefs = coefs4)
+
+
+#Combine them to track over time
+tbls <- vector('list', length = 7)
+
+lapply(1:7, FUN = function(aa){
+  t1 <- as.character(tbl1[, aa])
+  t2 <- as.character(tbl2[, aa])
+  t3 <- as.character(tbl3[, aa])
+  t4 <- as.character(tbl4[, aa])
+  temp <- cbind(t1, t2, t3, t4)
+  temp <- as.data.frame(temp)
+  row.names(temp) <- row.names(tbl1)
+  names(temp) <- c("2011", '2012', '2013', '2014')
+  names(tbl2) <- gsub("/", "_", names(tbl2))
+  filename <- paste0('figs/', names(tbl2)[aa], '.csv')
+  write.csv(temp, file = filename)
 })
 
-c1_table <- ldply(c1_table)
-names(c1_table) <- c('port', rownames(coefs1[[1]]))
-c1_table <- t(c1_table)
-c1_table <- as.data.frame(c1_table)
+{
+  tbl1
+}
 
-names(c1_table) <- port_names
-c1_table <- c1_table[-1, ]
-names(c1_table) <- c("MOS/SF", "FTB", "EUR", "CC/B", "CHA",
-  "NEW", "AST", "ILW/NEW")
+for(ii in 1:7){
+  l
+}
 
+
+
+# format_coefs <- function(coefs1)
+
+
+# coefs1 <- lapply(coefs1, FUN = function(xx){
+#   xx$value = paste(formatC(xx$coefs, digits = 5, format = 'f'), xx$significance)
+#   return(xx)
+# } )
+
+# c1_table <- lapply(coefs1, FUN = function(xx){
+#   return(xx$value)
+# })
+
+
+# #Try foramtting this shit in xtable
+# c1_table <- lapply(coefs1, FUN = function(xx){
+#   return(xx$value)
+# })
+
+# c1_table <- ldply(c1_table)
+# names(c1_table) <- c('port', rownames(coefs1[[1]]))
+# c1_table <- t(c1_table)
+# c1_table <- as.data.frame(c1_table)
+
+# names(c1_table) <- port_names
+# c1_table <- c1_table[-1, ]
+# names(c1_table) <- c("MOS/SF", "FTB", "EUR", "CC/B", "CHA",
+#   "NEW", "ILW/NEW")
+
+
+
+
+
+
+coefs1
+
+
+
+#Calculate difference between tables
+# lapply(1:8, FUN = function(xx){
+#   temp <- coefs50[[xx]]$coefs - coefs1[[xx]]$coefs
+#   coefs1[[xx]]$change <- temp
+#   return(coefs1[[xx]])
+# } )
 
 
 xtable(c1_table)
