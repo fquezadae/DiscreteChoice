314bd4ca3b77029e50faf01b5601d97f2a853158
Peter Kuriyama <ptrkrym@uw.edu>
2017-10-31 08:04:05 -0700

    debug

--- R/sampled_rums.R
+++ R/sampled_rums.R
@@ -168,16 +168,16 @@ sampled_rums <- function(data_in = filt_clusts, the_port = "ASTORIA / WARRENTON"
   #-----------------------------------------------------------------------------  
   #Process dummy variables
 # pd <- process_dummys2(xx = 1, td2 = td1, dat1 = dat)    
-# browser()  
-  # yy <- process_dummys2(xx = 1, td2 = td1, dat1 = dat, hab_dist = habit_distance, n_cost = net_cost)
 
+  # yy <- process_dummys2(xx = 3, td2 = td1, dat1 = dat, hab_dist = habit_distance, n_cost = net_cost)
+# browser()
   dummys2 <- foreach::foreach(ii = 1:nrow(td1), 
     .packages = c("dplyr", 'lubridate', 'ch4')) %dopar% 
       process_dummys2(xx = ii, td2 = td1, dat1 = dat, hab_dist = habit_distance, n_cost = net_cost)
   stopCluster(cl)
 
   print("Done calculating dummys and revenues")    
-# browser()
+
   td1 <- ldply(dummys2)
 
   #Check to see that this value is right 
