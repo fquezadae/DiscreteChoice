78047f925c0e4f306f92d6f4a50308fa0a614722
peterkuriyama <ptrkrym@uw.edu>
2017-11-05 16:27:36 -0800

    update counting

--- figs/ch4_fig5_vess_shifts_anova.R
+++ figs/ch4_fig5_vess_shifts_anova.R
@@ -18,16 +18,14 @@ deltas <- d_lat %>% left_join(d_long, by = 'drvid')
 #For the vessels that remained, what were the shifts in effort
 deltas <- deltas[which(is.na(deltas$delta_lat) == FALSE), ]
 
-diff_ports <- deltas %>% filter(delta_lat >= .5 | delta_lat <= -.5)
-deltas <- deltas %>% filter(delta_lat < .5, delta_lat > -.5)
+diff_ports <- deltas %>% filter(delta_lat >= 1 | delta_lat <= -1)
+deltas <- deltas %>% filter(delta_lat < 1, delta_lat > -1)
 deltas <- deltas %>% as.data.frame
 
 #------------------------------------------------------------------------
 #Run ANOVAs for each vessel that remained
 
-one_vess <- tows_clust %>% filter(drvid == 1037785)
 load("output/lat_long_anova.Rdata")
-head(deltas)
 
 deltas <- deltas %>% left_join(lat_long_anova, by = 'drvid')
 # add significant long color of transparent blue
@@ -36,6 +34,13 @@ deltas[which(deltas$sig_long == T), "sig_long_color"] <- adjustcolor('red', alph
 
 deltas$sig_lat_color <- adjustcolor('black', alpha.f = 0)
 deltas[which(deltas$sig_lat == T), "sig_lat_color"] <- adjustcolor('blue', alpha.f = .5)
+
+#------------------------------------------------------------------------
+#Table
+table(deltas$sig) / sum(table(deltas$sig))
+
+
+
 #------------------------------------------------------------------------
 #Figure
 
@@ -50,8 +55,8 @@ par(mar = c(0, 0, 0, 0), oma = c(3, 4, 1, 1), mgp = c(0, .5, 0))
 #Plot 1
 plot(deltas$delta_long, deltas$delta_lat, pch = 19, 
   col = deltas$sig_long_color, cex = 1, ann = F, axes = F,
-  xaxs = 'i', yaxs = 'i', xpd = T, ylim = c(-.5, .5),
-  xlim = c(-.4, .4))
+  xaxs = 'i', yaxs = 'i', xpd = T, ylim = c(-1, 1),
+  xlim = c(-.5, .5))
 points(deltas$delta_long, deltas$delta_lat, pch = 19, col = deltas$sig_lat_color, cex = 1)
 
 #add nonsignificant points, open no color
@@ -63,24 +68,24 @@ abline(v = 0, lty = 2)
 abline(h = 0, lty = 2)
 axis(side = 1, cex = 1.5)
 axis(side = 2, las = 2)
-# abline(v = .37)
-# /box()
 
 #------------------------
 #Plot 2
-hist1 <- hist(deltas$delta_long, breaks = seq(-.4, .4, by = .05), plot = F)
+hist1 <- hist(deltas$delta_long, breaks = seq(-.5, .5, by = .05), plot = F)
 
 barplot(hist1$density, space = 0, ylim = c(0, max(hist1$density)), axes = F, 
-  xlim = c(0.5, 15.5), col = adjustcolor('red', alpha.f = .5))
+  xlim = c(0.5, length(hist1$density) - .5), col = adjustcolor('red', alpha.f = .5))
 # box()
 # axis(side = 1)
 
 #------------------------
 #plot 3
-hist2 <- hist(deltas$delta_lat, breaks = seq(-.5, .5, by = .05), plot = F)
+hist2 <- hist(deltas$delta_lat, breaks = seq(-1, 1, by = .05), plot = F)
 barplot(hist2$density, space = 0, xlim = c(0, max(hist2$density)), axes = F, horiz = T,
-  ylim = c(0.5, 17.5), col = adjustcolor('blue', alpha.f = .5))
+  ylim = c(0.5, length(hist2$density) - .5), col = adjustcolor('blue', alpha.f = .5))
 
 mtext(side = 1, outer = T, "Change in longitude", adj = .3, line = 1.7, cex = 1.2)
 mtext(side = 2, outer = T, "Change in latitude", adj = .3, line = 2.2, cex = 1.2)
+
+
 dev.off()
