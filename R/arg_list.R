d333dec856816cee1edfdc76cd4cfd755e07d006
peterkuriyama <ptrkrym@uw.edu>
2017-12-04 13:12:06 -0800

    update argument list

---
+++ R/arg_list.R
@@ -0,0 +1,16 @@
+#---------------------------------------------------------------------------------
+#Function to generate list of arguments
+#' @param m_y minimum year
+#' @export
+
+arg_list <- function(m_y = 2007, f_y = 2009, nhauls_sampled = 50,
+                     ncores = 8, seed = the_seed, r_c = 50, r_s = 100, ports = the_ports, 
+                     h_d = the_hd,
+                     dyz = the_days, quota_species = c("Canary Rockfish"), 
+                     n_c = "trev"){
+  outs <- list(m_y = m_y, f_y = f_y, nhauls_sampled = nhauls_sampled,
+                     ncores = ncores, seed = seed, r_c = r_c, r_s = r_s, 
+                     ports = ports, h_d = h_d,
+                     dyz = dyz, quota_species = quota_species, n_c = n_c)
+  return(outs)
+}
