2b60a0daa72a090023e1bc98d631f22b5a6dae8b
Peter Kuriyama <ptrkrym@uw.edu>
2017-12-15 10:22:32 -0800

    add in coefficients

---
+++ R/process_coefficients.R
@@ -0,0 +1,24 @@
+#' Function to process coefficients
+#' 
+#' Function to process coefficients and add in relevant arguments
+#' 
+#' @param filename Name of file
+#' @param dir Path to directory
+#' @export
+
+process_coefficients <- function(filename, dir = "/Volumes/udrive/"){
+  #
+
+  load(paste0(dir, filename))
+  coefs1 <- melt(coefs, id.vars = c('coefs', 'p_values', 'significance'))
+  coefs1 <- plyr::rename(coefs1, c("L1" = "port"))
+  
+  #Parse filename and add details in to coefs1
+  filename_args <- unlist(strsplit(filename, split = "_"))
+  coefs1$price_multiplier <- gsub("coefs", "", filename_args[1])
+  coefs1$year <- gsub("focyr", "", filename_args[4])
+  coefs1$hdist <- gsub("hdist", "", filename_args[8])
+  coefs1$spp <- gsub("qspecies", "", filename_args[10])
+  coefs1$spp <- gsub('.Rdata', "", coefs1$spp)	
+  return(coefs1)
+}
