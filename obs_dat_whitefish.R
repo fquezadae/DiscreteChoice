0a947804822c5720a4e734bfa9155fce1c423940
Peter Kuriyama <ptrkrym@uw.edu>
2018-01-19 11:58:48 -0800

    update

--- obs_dat_whitefish.R
+++ obs_dat_whitefish.R
@@ -54,16 +54,16 @@ load("//udrive.uw.edu//udrive//tows_clust_1010.Rdata")
 the_ports <- list("EUREKA", "CHARLESTON (COOS BAY)", 
   "NEWPORT", "ASTORIA / WARRENTON")
 
-the_seed <- 10
+the_seed <- 1000
 the_days <- 30
 the_hd <- 5.1 #habit distance
 
 quota_species = c("Canary Rockfish")
 
-
 #-----------------------------------------
-a_l <- arg_list(ncores = 6, seed = the_seed, r_c = 10, r_s = 100, ports = the_ports,
-                     h_d = the_hd, dyz = the_days, quota_species = quota_species, n_c = 'trev')
+a_l <- arg_list(ncores = 8, seed = the_seed, r_c = 10, r_s = 100, ports = the_ports,
+                     h_d = the_hd, dyz = the_days, quota_species = quota_species, n_c = 'trev',
+                nhauls_sampled= 100)
 run_six_years(the_args = a_l)
 
 
