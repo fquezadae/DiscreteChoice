0732eb51a66450abf39d6ba8c5d0fca259d74b13
peterkuriyama <ptrkrym@uw.edu>
2018-06-22 17:50:40 -0700

    add cases

--- obs_dat_noaa.R
+++ obs_dat_noaa.R
@@ -37,8 +37,6 @@ states_map <- map_data("state")
 # load("//udrive.uw.edu//udrive//tows_clust_1010.Rdata")
 # load("//udrive.uw.edu//udrive//tows_clust_0620.Rdata")
 load('output//tows_clust_0621.Rdata')
-tows_clust %>% filter(set_year == 2011) %>% distinct(haul_id, .keep_all = T) %>%
-    group_by(trip_id) %>% summarize(avg_tgow = mean(tgow_rev))
 
 #------------------------------------------------------------------------------
 #Sensitivity Runs with different quota species
@@ -56,35 +54,44 @@ quota_species = c("Canary Rockfish", "Darkblotched Rockfish",
 
 #-----------------------------------------
 
-##1: CSDE Cluster: Run with Coefficient of 1 and total revenues for all years 
+##1: Run with Coefficient of 1 and total revenues for all years 
 a_l <- arg_list(ncores = 10, seed = the_seed, r_c = 1, r_s = 100, ports = the_ports,
                      h_d = the_hd, dyz = the_days, quota_species = quota_species, 
                      n_c = 'trev',
                 nhauls_sampled = 50)
 run_six_years(the_args = a_l, years = 2009:2014)
 
-##2: Blackfish Run: with Coefficient of 100 and quota costs for 2011:2014 for all 
-#constraining species
-a_l <- arg_list(ncores = 8, seed = the_seed, r_c = 100, r_s = 100, ports = the_ports,
+#2: Run with qcos and coefficient of 1
+a_l <- arg_list(ncores = 10, seed = the_seed, r_c = 1, r_s = 100, ports = the_ports,
                      h_d = the_hd, dyz = the_days, quota_species = quota_species, 
                      n_c = 'qcos',
                 nhauls_sampled = 50)
 run_six_years(the_args = a_l, years = 2011:2014)
-#Because there's no difference in the model looking at Canary only
 
-
-##3: Some computer: Run with a different seed to see if results change
-a_l <- arg_list(ncores = 10, seed = the_seed + 10, r_c = 1, r_s = 100, 
-      ports = the_ports,
+#3 Run with qcost and coefficient of 5
+a_l <- arg_list(ncores = 10, seed = the_seed, r_c = 5, r_s = 100, ports = the_ports,
                      h_d = the_hd, dyz = the_days, quota_species = quota_species, 
-                     n_c = 'trev',
+                     n_c = 'qcos',
                 nhauls_sampled = 50)
-run_six_years(the_args = a_l, years = 2009:2014)
+run_six_years(the_args = a_l, years = 2011:2014)
 
-#4: Run with 75 hauls - CSDE
-a_l <- arg_list(ncores = 10, seed = the_seed + 10, r_c = 1, r_s = 100, 
-                ports = the_ports,
-                h_d = the_hd, dyz = the_days, quota_species = quota_species, 
-                n_c = 'trev',
-                nhauls_sampled = 75)
-run_six_years(the_args = a_l, years = 2009:2014)
+#4: Run with coefficient of 10
+a_l <- arg_list(ncores = 10, seed = the_seed, r_c = 10, r_s = 100, ports = the_ports,
+                     h_d = the_hd, dyz = the_days, quota_species = quota_species, 
+                     n_c = 'qcos',
+                nhauls_sampled = 50)
+run_six_years(the_args = a_l, years = 2011:2014)
+
+#5: Coefficient of 50
+a_l <- arg_list(ncores = 10, seed = the_seed, r_c = 50, r_s = 100, ports = the_ports,
+                     h_d = the_hd, dyz = the_days, quota_species = quota_species, 
+                     n_c = 'qcos',
+                nhauls_sampled = 50)
+run_six_years(the_args = a_l, years = 2011:2014)
+
+#6: Coefficient of 100
+a_l <- arg_list(ncores = 10, seed = the_seed, r_c = 100, r_s = 100, ports = the_ports,
+                     h_d = the_hd, dyz = the_days, quota_species = quota_species, 
+                     n_c = 'qcos',
+                nhauls_sampled = 50)
+run_six_years(the_args = a_l, years = 2011:2014)
