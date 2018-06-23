3fde48fba0f1da50a2c861961f9d753989745c5e
Peter Kuriyama <ptrkrym@uw.edu>
2018-06-23 09:39:21 -0700

    comment out email part

--- R/run_six_years.R
+++ R/run_six_years.R
@@ -23,8 +23,8 @@ run_six_years <- function(the_args = a_l, years = 2009:2014){
                        ports = the_args$ports, h_d = the_args$h_d,
                        dyz = the_args$dyz, quota_species = the_args$quota_species, 
                        n_c = "trev")  
-    sendmail(from = from_email, to = to_email, subject = paste("2009", subject),
-        msg = body, control = mailControl)
+    # sendmail(from = from_email, to = to_email, subject = paste("2009", subject),
+    #     msg = body, control = mailControl)
   }
 
   
@@ -36,8 +36,8 @@ run_six_years <- function(the_args = a_l, years = 2009:2014){
                          ports = the_args$ports, h_d = the_args$h_d,
                          dyz = the_args$dyz, quota_species = the_args$quota_species, 
                          n_c = "trev")  
-    sendmail(from = from_email, to = to_email, subject = paste("2010", subject),
-        msg = body, control = mailControl)
+    # sendmail(from = from_email, to = to_email, subject = paste("2010", subject),
+    #     msg = body, control = mailControl)
   }
   
 
@@ -49,8 +49,8 @@ run_six_years <- function(the_args = a_l, years = 2009:2014){
                        ports = the_args$ports, h_d = the_args$h_d,
                        dyz = the_args$dyz, quota_species = the_args$quota_species, 
                        n_c = the_args$n_c)  
-    sendmail(from = from_email, to = to_email, subject = paste("2011", subject),
-        msg = body, control = mailControl)
+    # sendmail(from = from_email, to = to_email, subject = paste("2011", subject),
+    #     msg = body, control = mailControl)
   }
   
   if(2012 %in% years){
@@ -61,8 +61,8 @@ run_six_years <- function(the_args = a_l, years = 2009:2014){
                        ports = the_args$ports, h_d = the_args$h_d,
                        dyz = the_args$dyz, quota_species = the_args$quota_species, 
                        n_c = the_args$n_c)  
-    sendmail(from = from_email, to = to_email, subject = paste("2012", subject),
-        msg = body, control = mailControl)
+    # sendmail(from = from_email, to = to_email, subject = paste("2012", subject),
+    #     msg = body, control = mailControl)
   }
   
   if(2013 %in% years){
@@ -73,8 +73,8 @@ run_six_years <- function(the_args = a_l, years = 2009:2014){
                        ports = the_args$ports, h_d = the_args$h_d,
                        dyz = the_args$dyz, quota_species = the_args$quota_species, 
                        n_c = the_args$n_c)  
-     sendmail(from = from_email, to = to_email, subject = paste("2013", subject),
-        msg = body, control = mailControl)
+     # sendmail(from = from_email, to = to_email, subject = paste("2013", subject),
+     #    msg = body, control = mailControl)
   }
   
   if(2014 %in% years){
@@ -85,8 +85,8 @@ run_six_years <- function(the_args = a_l, years = 2009:2014){
                        ports = the_args$ports, h_d = the_args$h_d,
                        dyz = the_args$dyz, quota_species = the_args$quota_species, 
                        n_c = the_args$n_c)  
-     sendmail(from = from_email, to = to_email, subject = paste("2014", subject),
-        msg = body, control = mailControl)
+     # sendmail(from = from_email, to = to_email, subject = paste("2014", subject),
+     #    msg = body, control = mailControl)
   }
   
 }
